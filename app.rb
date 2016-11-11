b1bf51d0e05c283a44d94bada8dc4c06f52a2759
Georgia Mills <georgee_123@hotmail.com>
2016-11-11 12:59:46 +0000

    added footer and contact page

--- app.rb
+++ app.rb
@@ -9,7 +9,25 @@ class GracePrince < Sinatra::Base
     erb :index
   end
 
+  get '/group' do
+    'oooooops'
+  end
 
+  get '/group/one' do
+    erb :'./group/one'
+  end
+
+  get '/group/two' do
+    erb :'./group/two'
+  end
+
+  get '/group/three' do
+    erb :'./group/three'
+  end
+
+  get '/contact' do
+    erb :'./footer/contact'
+  end
 
 
   # start the server if ruby file executed directly
