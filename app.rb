ff21e5a8d010308bacead93003ef513df40941ff
Georgia Mills <georgee_123@hotmail.com>
2016-11-11 12:35:42 +0000

    added video_view and index erb

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require 'sinatra/base'
 
 class GracePrince < Sinatra::Base
   get '/' do
+    erb :video_view
+  end
+
+  get '/index' do
     erb :index
   end
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
